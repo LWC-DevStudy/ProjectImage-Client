e0445f5514fed52b72f03b8e261f0588db801131
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-08-07 13:11:11 +0900

    💄Input, Login 중간 작업

--- image-community/src/elements/Input.js
+++ image-community/src/elements/Input.js
@@ -3,9 +3,76 @@ import React from 'react';
 
 // style
 import styled from 'styled-components';
+import { borderBox } from '../shared/style';
+
+import Text from './Text';
+import Grid from './Grid';
 
 const Input = (props) => {
-  return <></>;
+  const { label, placeholder, _onChange, type, multiline, value, margin, width } = props;
+  
+  if (multiline) {
+    return (
+      <>
+        {/* <Grid> */}
+          {/* {label && <Text margin="0px">{label}</Text>} */}
+          <>{label}</>
+          <ElTextarea
+            value={value}
+            rows={10}
+            placeholder={placeholder}
+            onChange={_onChange}
+            ></ElTextarea>
+        {/* </Grid> */}
+      </> 
+    );
+  }
+
+  return (
+    <>
+      {/* <Grid> */}
+        {/* {label && <Text margin="0px">{label}</Text>} */}
+        <>{label}</>
+        <ElInput 
+          width={width} 
+          margin={margin} 
+          type={type} 
+          placeholder={placeholder} 
+          onChange={_onChange}/>
+      {/* </Grid> */}
+    </>
+  )
 };
+
+Input.defaultProps = {
+  multiline: false,
+  label: false,
+  placeholder: "텍스트를 입력해주세요.",
+  type: "text",
+  value: "",
+  margin: 0,
+  padding: 0,
+  width: '100%',
+  _onChange: () => {},
+  
+};
+
+const ElTextarea = styled.textarea`
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  border: 1px solid black;
+  width: ${(props) => props.width};
+  padding: 12px 4px;
+  
+`;
+
+const ElInput = styled.input`
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  border: 1px solid black;
+  width: ${(props) => props.width};
+  padding: 12px 4px;
+  
+`;
 
 export default Input;
