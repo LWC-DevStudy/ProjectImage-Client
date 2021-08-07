01e1a3f8c63e7c3ae8848109c9ae78ef20853725
sunm-in <sunm309@naver.com>
2021-08-07 12:08:52 +0900

    💄Button, Text 작업 완료

--- image-community/src/elements/Text.js
+++ image-community/src/elements/Text.js
@@ -4,8 +4,27 @@ import React from 'react';
 // style
 import styled from 'styled-components';
 
-const Text = (props) => {
-  return <></>;
+const Text = ({ children, ...props }) => {
+  return <TextStyle {...props}>{children}</TextStyle>;
 };
+
+Text.defaultProps = {
+  color: 'white',
+  addstyle: () => {},
+};
+
+const TextStyle = styled.p`
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => `rgb(${props.theme.palette[props.color]})`};
+  line-height: ${(props) => props.lineHeight};
+  text-indent: ${(props) => props.textIndent};
+  font-weight: ${(props) => props.fontWeight};
+  margin: ${(props) => props.margin};
+  text-align: ${(props) => props.textAlign};
+  word-break: break-all;
+  white-space: pre-line;
+
+  ${(props) => props.addstyle()};
+`;
 
 export default Text;
